b4872e33be8350d09f446f3ec369e416bdaf3d0b
tvbgo <tomas.gomes@rdstation.com>
2021-05-25 15:57:25 -0300

    added victory condition

--- gift.rb
+++ gift.rb
@@ -3,7 +3,7 @@ puts ""
 puts ""
 puts ""
 
-
+puts "RÁPIDO! GRITE 'O MILKSHAKE É TODO MEU!' PARA SINZALIZAR QUE VOCÊ CHEGOU AO FIM E PODER RESGATAR O SEU PRÊMIO!!!"
 puts "Parabéns! Você conquistou o maravilhoso e inegualável VALE MILKSHAKE! Caso prefira outra guloseima o valor do vale é de R$35 valido
 para qualquer item do IFOOD!"
 
